6d2bfca232c72b6a367fa9e493f025f57d2b47a9
atolmie <atolmie22@gmail.com>
2019-07-08 17:11:37 -0500

    store done

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux';
+import store from './redux/store';
 
 
 ReactDOM.render(
-<App />, 
+<Provider store={store}>
+</Provider>,
 document.getElementById('root')
 );
 
