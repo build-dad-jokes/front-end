f6ecd032ac54c5115752c12bcca076a95898b79b
atolmie <atolmie22@gmail.com>
2019-07-09 17:42:29 -0500

    sign up form done

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/Home";
 import LoginForm from "./components/LoginForm/LoginForm";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Route path="/" exact={true} component={Home} />
-                <Route path="/login" component={LoginForm} />
+                <div>
+                    <Route path="/" exact={true} component={Home} />
+                    <Route path="/login" component={LoginForm} />
+                    <Route path="/signup" component={SignUpForm} />
+                </div>
             </BrowserRouter>
         );
     }
---
+++ my-react-app/src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,90 @@
+import React from 'react';
+
+class SignUpForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            passwordConfirmation: '',
+            
+        }
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <h1>Join the Dad Jokes Community! </h1>
+
+                <div className="form-group">
+                    <label className="control-label">Username</label>
+                    <input
+                        value={this.state.username}
+                        onChange={this.onChange}
+                        type="text"
+                        name="username"
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Email</label>
+                    <input
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        type="text"
+                        name="email"
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Password</label>
+                    <input
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        type="text"
+                        name="password"
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Password Confirmation</label>
+                    <input
+                        value={this.state.passwordConfirmation}
+                        onChange={this.onChange}
+                        type="text"
+                        name="passwordconfirmation"
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <button className="btn">
+                        Sign Up
+                        </button>
+                </div>
+            </form>
+
+
+
+
+        );
+    }
+}
+
+export default SignUpForm;
